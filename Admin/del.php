6e96e33878c7771ed9cc9879e84b92b3f5f8e185
SahilAdk5091 <np03cs4s210159@heraldcollege.edu.np>
2023-03-31 09:27:40 +0545

    Deleting all the files

--- Admin/del.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'connection.php';
-$id = $_GET['id'];
-$deletequery = "delete from user where id=$id";
-$query = mysqli_query($con,$deletequery);
-header('location:displayadmin.php');
-?>
--- Admin/deldetail.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'connection.php';
-$id = $_GET['id'];
-$deletequery = "delete from cropdetails where id=$id";
-$query = mysqli_query($con,$deletequery);
-header('location:displayinadmin.php');
-?>
